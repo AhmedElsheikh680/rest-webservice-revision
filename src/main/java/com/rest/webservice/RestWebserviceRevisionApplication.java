fda3cd484245af262fc9d98e11440a52aa8ca7ae
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-06-14 15:34:10 +0200

    init project

---
+++ src/main/java/com/rest/webservice/RestWebserviceRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.rest.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebserviceRevisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWebserviceRevisionApplication.class, args);
+    }
+
+}
